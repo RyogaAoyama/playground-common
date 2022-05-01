537f9dbb3d1b9407f1f032a7f750c31b8282a535
RyogaAoyama <ryogaaoym0114@gmail.com>
2022-05-01 16:35:29 +0900

    初回コミット

---
+++ sh/deploy-net.sh
@@ -0,0 +1,20 @@
+ENV=$1
+PROFILE=$2
+
+if [ "${ENV}" = "" ];
+then
+  BIN_ENV=dev
+fi
+
+if [ "${PROFILE}" = "" ];
+then
+  BIN_ENV=default
+fi
+
+TEMPLATE_PATH='./cloudformation/Networks.yml'
+
+# 環境変数をインポート
+. ./cloudformation/config/parameters.txt
+
+# デプロイ
+rain deploy ${TEMPLATE_PATH} ${NETWORK_STACK_NAME} -y -p ${PROFILE} --params Env=${Env},ServiceName=${SERVICE_NAME},SystemName=${SYSTEM_NAME}
